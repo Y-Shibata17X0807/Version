e3555d6bbcaa3ce84d96ba45a4bbd7854f0a60ca
Y-Shibata <tongpoo2010@gmail.com>
2018-05-02 08:48:24 +0900

    sln追加（5月2日）

---
+++ 原価試算作成ツール/原価試算書作成ツール/Form1.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace 原価試算書作成ツール
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Output = new System.Windows.Forms.TextBox();
+            this.Dialog2 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.CreateBtn = new System.Windows.Forms.Button();
+            this.Dialog1 = new System.Windows.Forms.Button();
+            this.Input = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // Output
+            // 
+            this.Output.Font = new System.Drawing.Font("メイリオ", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.Output.Location = new System.Drawing.Point(12, 125);
+            this.Output.Name = "Output";
+            this.Output.Size = new System.Drawing.Size(499, 27);
+            this.Output.TabIndex = 2;
+            // 
+            // Dialog2
+            // 
+            this.Dialog2.Font = new System.Drawing.Font("メイリオ", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.Dialog2.Location = new System.Drawing.Point(517, 125);
+            this.Dialog2.Name = "Dialog2";
+            this.Dialog2.Size = new System.Drawing.Size(44, 27);
+            this.Dialog2.TabIndex = 5;
+            this.Dialog2.Text = "...";
+            this.Dialog2.UseVisualStyleBackColor = true;
+            this.Dialog2.Click += new System.EventHandler(this.Dialog2Btn_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("メイリオ", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label3.Location = new System.Drawing.Point(12, 102);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(289, 20);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "作成したExcelシートの保存先を選択して下さい";
+            // 
+            // CreateBtn
+            // 
+            this.CreateBtn.Font = new System.Drawing.Font("メイリオ", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.CreateBtn.Location = new System.Drawing.Point(67, 174);
+            this.CreateBtn.Name = "CreateBtn";
+            this.CreateBtn.Size = new System.Drawing.Size(416, 39);
+            this.CreateBtn.TabIndex = 9;
+            this.CreateBtn.Text = "試算書作成開始";
+            this.CreateBtn.UseVisualStyleBackColor = true;
+            this.CreateBtn.Click += new System.EventHandler(this.CreateBtn_Click);
+            // 
+            // Dialog1
+            // 
+            this.Dialog1.Font = new System.Drawing.Font("メイリオ", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.Dialog1.Location = new System.Drawing.Point(517, 47);
+            this.Dialog1.Name = "Dialog1";
+            this.Dialog1.Size = new System.Drawing.Size(44, 27);
+            this.Dialog1.TabIndex = 11;
+            this.Dialog1.Text = "...";
+            this.Dialog1.UseVisualStyleBackColor = true;
+            this.Dialog1.Click += new System.EventHandler(this.Dialog1_Click);
+            // 
+            // Input
+            // 
+            this.Input.Font = new System.Drawing.Font("メイリオ", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.Input.Location = new System.Drawing.Point(12, 47);
+            this.Input.Multiline = true;
+            this.Input.Name = "Input";
+            this.Input.Size = new System.Drawing.Size(499, 28);
+            this.Input.TabIndex = 10;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("メイリオ", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label1.Location = new System.Drawing.Point(12, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(230, 20);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "見積もり情報シートを選択して下さい";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(587, 225);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Dialog1);
+            this.Controls.Add(this.Input);
+            this.Controls.Add(this.CreateBtn);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.Dialog2);
+            this.Controls.Add(this.Output);
+            this.Name = "Form1";
+            this.Text = "原価試算作成ツール V1.0.0.0 Pre-Alpha";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox Output;
+        private System.Windows.Forms.Button Dialog2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button CreateBtn;
+        private System.Windows.Forms.Button Dialog1;
+        private System.Windows.Forms.TextBox Input;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ 原価試算作成ツール/原価試算書作成ツール/Form1.cs
@@ -0,0 +1,362 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+using NPOI.SS.UserModel;
+using System.Text.RegularExpressions;
+using System.Linq;
+
+namespace 原価試算書作成ツール
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private string[] ExcelInfo = new string[10];
+        private string[] RepNum = new string[10];
+        private string DeskPath = Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory);
+
+        private Regex r =
+        new Regex("[\\x00-\\x1f<>:\"/\\\\|?*]" + "|^(CON|PRN|AUX|NUL|COM[0-9]|LPT[0-9]|CLOCK\\$)(\\.|$)" + "|[\\. ]quot;", RegexOptions.IgnoreCase);
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Output.Text = Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory);
+            RepNum[0] = "-A001";
+            RepNum[1] = "-C001";
+            RepNum[2] = "-Q1";
+            RepNum[3] = "-C101";
+        }
+
+        private void Dialog1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+
+            ofd.InitialDirectory = @"\\Gserver\002_各種物件";
+            ofd.RestoreDirectory = true;
+            ofd.Title = "見積もり情報シートを選択して下さい";
+            ofd.Filter = "Excel 97-2003形式(*.xls)|*.xls|Excelワークシート(*.xlsx)|*.xlsx";
+            //ダイアログを表示する
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                //OKボタンがクリックされたとき、選択されたファイル名を表示する
+                Console.WriteLine(ofd.FileName);
+                Input.Text = ofd.FileName;
+            }
+            ofd.Dispose();
+        }
+
+        private void Dialog2Btn_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+            fbd.Description = "フォルダを指定してください。";
+            if (fbd.ShowDialog(this) == DialogResult.OK)
+            {
+                //選択されたフォルダを表示する
+                Console.WriteLine(fbd.SelectedPath);
+                Output.Text = fbd.SelectedPath;
+            }
+            fbd.Dispose();
+        }
+
+        private void CreateBtn_Click(object sender, EventArgs e)
+        {
+            if (check() == true)
+                if (Copy() == true)
+                {
+                    Console.WriteLine("コピーに成功");
+                    if (ReadEx() == true)
+                    {
+                        var di = new DirectoryInfo(DeskPath + @"\原価試算書フォルダ");
+                        var Num = 1;
+                        if (File.Exists(DeskPath + @"\原価試算書フォルダ\【" + ExcelInfo[0] + " " + "-C00" + Num + "】" + "製造原価試算書.xlsx") == true)
+                        {
+                            File.Move(DeskPath + @"\原価試算書フォルダ\HR40-C001_製造原価試算書_T.xlsx",
+                            DeskPath + @"\原価試算書フォルダ\【" + ExcelInfo[0] + " " + "-C00" + Num++ + "】" + "製造原価試算書.xlsx");
+                        }
+                        if (WriteExManu() == true)
+                        {
+                            File.Move(DeskPath + @"\原価試算書フォルダ\HR40-C001_製造原価試算書_T.xlsx",
+                            DeskPath + @"\原価試算書フォルダ\【" + ExcelInfo[0] + " " + "-C001" + "】" + "製造原価試算書.xlsx");
+                        }
+                        else
+                        {
+                            MessageBox.Show("製造原価試算書の作成に失敗しました。");
+                            return;
+                        }
+                        if (WriteExDev() == true)
+                        {
+                            File.Move(DeskPath + @"\原価試算書フォルダ\HR209-C101_開発原価試算書_T.xlsx",
+                            DeskPath + @"\原価試算書フォルダ\【" + ExcelInfo[0] + " " + "-C101" + "】" + "開発原価試算書.xlsx");
+                            MessageBox.Show("原価試算書の作成が完了しました。");
+                        }
+                        else
+                        {
+                            MessageBox.Show("開発原価試算書の作成に失敗しました。");
+                            return;
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("見積もり情報シートからデータを読み込むのに失敗しました");
+                        return;
+                    }
+                }
+                else
+                {
+                    return;
+                }
+            File.Delete(DeskPath + @"\原価試算書フォルダ\HR40-C001_製造原価試算書_T.xlsx");
+            File.Delete(DeskPath + @"\原価試算書フォルダ\HR209-C101_開発原価試算書_T.xlsx");
+        }
+
+        private bool Copy()
+        {
+            try
+            {
+                string[] dirs = Directory.GetFiles(@"\\Gserver\999_個人フォルダ\柴田\原紙テスト\", "【原紙】HR40-C001*.xlsx1");
+                File.Copy(dirs[0], DeskPath + @"\HR40-C001_製造原価試算書.xlsx");
+
+                string[] dirs1 = Directory.GetFiles(@"\\Gserver\999_個人フォルダ\柴田\原紙テスト\", "【原紙】HR209-C101*.xlsx");
+                File.Copy(dirs1[0], DeskPath + @"\HR209-C101_開発原価試算書.xlsx");
+
+                DirectoryInfo di = new DirectoryInfo(DeskPath + @"\原価試算書フォルダ");
+                di.Create();
+                File.Move(DeskPath + @"\HR40-C001_製造原価試算書.xlsx",
+                    DeskPath + @"\原価試算書フォルダ\HR40-C001_製造原価試算書.xlsx");
+                File.Move(DeskPath + @"\HR209-C101_開発原価試算書.xlsx",
+                    DeskPath + @"\原価試算書フォルダ\HR209-C101_開発原価試算書.xlsx");
+                di = null;
+            }
+            catch (Exception ex)
+            {
+                if (ex is IOException)
+                {
+                    MessageBox.Show("既に原価試算書フォルダは存在します");
+
+                    return false;
+                }
+                if (ex is UnauthorizedAccessException)
+                {
+                    MessageBox.Show("ファイルへのアクセスが拒否されました");
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        private bool check()
+        {
+            if (Input.Text.Trim() == "")
+            {
+                MessageBox.Show("見積もり情報シートが未選択です");
+                return false;
+            }
+            if (Output.Text.Trim() == "")
+            {
+                MessageBox.Show("保存先が未選択です");
+                return false;
+            }
+            if (Directory.Exists(Input.Text) == false)
+            {
+                MessageBox.Show("選択した場所は存在しません");
+                return false;
+            }
+            if (Directory.Exists(Output.Text) == false)
+            {
+                MessageBox.Show("選択した保存場所は存在しません");
+                return false;
+            }
+
+            return true;
+        }
+
+        private bool ReadEx()//セルの情報読み取り
+        {
+            IRow row; ICell cell;
+            //var FilePath = Input.Text;
+            var FilePath = @"\\Gserver\999_個人フォルダ\柴田\原価試算作成ツール\課題\RFP18041301_見積り情報シート(西部電機様_バルコン本体).xls";
+            var work = WorkbookFactory.Create(FilePath);
+            var Sheet = work.GetSheetAt(0);
+
+            row = Sheet.GetRow(0);//行
+            cell = row.GetCell(11);
+            string[] sp = cell.ToString().Split('：');//開発受付番号を全角「：」から分割
+            ExcelInfo[0] = sp[1].Substring(0, 8);//開発工番のみ
+
+            row = Sheet.GetRow(8);
+            cell = row.GetCell(1);
+            ExcelInfo[1] = cell.ToString();//件名
+
+            row = Sheet.GetRow(9);
+            cell = row.GetCell(1);
+            ExcelInfo[2] = cell.ToString();//顧客名
+
+            row = Sheet.GetRow(7);
+            cell = row.GetCell(1);
+            ExcelInfo[3] = "●";//見積もり種別（■）
+
+            row = Sheet.GetRow(7);
+            cell = row.GetCell(3);
+            ExcelInfo[4] = "○";//見積もり種別（□）
+
+            ExcelInfo[5] = "Ver.1";//バージョン
+
+            row = Sheet.GetRow(37);//行
+            cell = row.GetCell(3);
+            ExcelInfo[6] = cell.ToString();//営業想定単価（想定単価）
+
+            row = Sheet.GetRow(36);//行
+            cell = row.GetCell(1);
+            sp = cell.ToString().Split('想');//「想」から分割
+            ExcelInfo[7] = sp[0];//開発費
+
+            return true;
+        }
+
+        private bool WriteExManu()//製造原価試算書の書き込み用関数
+        {
+            try
+            {
+                var WritePath = Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory) + @"\原価試算書フォルダ\HR40-C001_製造原価試算書.xlsx";
+                var WWork = WorkbookFactory.Create(WritePath);
+                var WSheet = WWork.GetSheetAt(0);
+                var row = WSheet.GetRow(12);
+                var cell = row.GetCell(7);
+                cell.SetCellValue(ExcelInfo[0] + RepNum[2]);// -Q1
+
+                WSheet = WWork.GetSheetAt(0);
+                row = WSheet.GetRow(12);
+                cell = row.GetCell(4);
+                cell.SetCellValue(ExcelInfo[0] + RepNum[0]);// -A001
+
+                WSheet = WWork.GetSheetAt(0);
+                row = WSheet.GetRow(13);
+                cell = row.GetCell(6);
+                cell.SetCellValue(ExcelInfo[0] + RepNum[1]);// -C001
+
+                WSheet = WWork.GetSheetAt(0);//製品名
+                row = WSheet.GetRow(13);
+                cell = row.GetCell(2);
+                cell.CellStyle.ShrinkToFit = true;//全体を縮小して表示
+                cell.SetCellValue(ExcelInfo[1]);
+
+                WSheet = WWork.GetSheetAt(0);
+                row = WSheet.GetRow(15);
+                cell = row.GetCell(2);
+                cell.SetCellValue(ExcelInfo[2]);//顧客名
+
+                WSheet = WWork.GetSheetAt(0);
+                row = WSheet.GetRow(15);
+                cell = row.GetCell(6);
+                cell.SetCellValue(ExcelInfo[5]);//バージョン
+
+                WSheet = WWork.GetSheetAt(0);
+                row = WSheet.GetRow(20);
+                cell = row.GetCell(2);
+                cell.SetCellValue(ExcelInfo[3]);//試算区分「■」
+
+                WSheet = WWork.GetSheetAt(0);
+                row = WSheet.GetRow(21);
+                cell = row.GetCell(2);
+                cell.SetCellValue(ExcelInfo[4]);//試算区分「□」
+
+                WSheet = WWork.GetSheetAt(0);
+                row = WSheet.GetRow(20);
+                cell = row.GetCell(9);
+                cell.SetCellValue(ExcelInfo[6]);//営業想定単価
+
+                using (var fs = new FileStream(Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory) + @"\原価試算書フォルダ\HR40-C001_製造原価試算書_T.xlsx", FileMode.Create))
+                {
+                    WWork.Write(fs);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+                return false;
+            }
+            return true;
+        }
+
+        private bool WriteExDev()//開発原価試算書の書き込み用関数
+        {
+            try
+            {
+                var WritePath = Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory) + @"\原価試算書フォルダ\HR209-C101_開発原価試算書.xlsx";
+                var WWork = WorkbookFactory.Create(WritePath);
+                var WSheet = WWork.GetSheetAt(0);
+                var row = WSheet.GetRow(12);
+                var cell = row.GetCell(7);
+                cell.SetCellValue(ExcelInfo[0] + RepNum[2]);// -Q1
+
+                WSheet = WWork.GetSheetAt(0);
+                row = WSheet.GetRow(12);
+                cell = row.GetCell(4);
+                cell.SetCellValue(ExcelInfo[0] + RepNum[0]);// -A001
+
+                WSheet = WWork.GetSheetAt(0);
+                row = WSheet.GetRow(13);
+                cell = row.GetCell(6);
+                cell.SetCellValue(ExcelInfo[0] + RepNum[3]);// -C101
+
+                WSheet = WWork.GetSheetAt(0);//製品名
+                row = WSheet.GetRow(13);
+                cell = row.GetCell(2);
+                cell.CellStyle.ShrinkToFit = true;
+                cell.SetCellValue(ExcelInfo[1]);
+
+                WSheet = WWork.GetSheetAt(0);
+                row = WSheet.GetRow(15);
+                cell = row.GetCell(2);
+                cell.SetCellValue(ExcelInfo[2]);//顧客名
+
+                WSheet = WWork.GetSheetAt(0);
+                row = WSheet.GetRow(15);
+                cell = row.GetCell(6);
+                cell.SetCellValue(ExcelInfo[5]);//バージョン
+
+                WSheet = WWork.GetSheetAt(0);
+                row = WSheet.GetRow(20);
+                cell = row.GetCell(2);
+                cell.SetCellValue(ExcelInfo[3]);//試算区分「■」
+
+                WSheet = WWork.GetSheetAt(0);
+                row = WSheet.GetRow(21);
+                cell = row.GetCell(2);
+                cell.SetCellValue(ExcelInfo[4]);//試算区分「□」
+
+                WSheet = WWork.GetSheetAt(0);
+                row = WSheet.GetRow(20);
+                cell = row.GetCell(9);
+                cell.SetCellValue(ExcelInfo[7]);//営業想定単価
+                using (var fs = new FileStream(Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory) + @"\原価試算書フォルダ\HR209-C101_開発原価試算書_T.xlsx", FileMode.Create))
+                {
+                    WWork.Write(fs);
+                }
+            }
+            catch (Exception ex)
+            {
+                if (ex is IOException)
+                {
+                    MessageBox.Show("書き込み中にエラーが発生しました");
+                    return false;
+                }
+                else if (ex is FileNotFoundException)
+                {
+                    MessageBox.Show("書き込み先のExcelシートが見つかりませんでした。");
+                    return false;
+                }
+                else
+                {
+                    MessageBox.Show("不明なエラーが発生しました");
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
